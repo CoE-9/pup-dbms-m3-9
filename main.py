91132661291c17560fecd83494f91d22cae8bc0a
Lee094 <real.leearvib@gmail.com>
2015-08-13 14:51:30 +0800

    fourth commit

--- main.py
+++ main.py
@@ -10,49 +10,85 @@ JINJA_ENVIRONMENT = jinja2.Environment(
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
-# class Thesis(ndb.Model):
-# 	year = ndb.StringProperty(indexed=True)
-# 	thesis_title = ndb.StringProperty(indexed=True)
-# 	abstract = ndb.StringProperty(indexed=True)
-# 	adviser = ndb.StringProperty(indexed=True)
-# 	section = ndb.StringProperty(indexed=True)
-# 	date = ndb.DateTimeProperty(auto_now_add=True)
+class Thesis(ndb.Model):
+	year = ndb.StringProperty(indexed=True)
+	thesis_title = ndb.StringProperty(indexed=True)
+	abstract = ndb.StringProperty(indexed=True)
+	adviser = ndb.StringProperty(indexed=True)
+	section = ndb.StringProperty(indexed=True)
+	date = ndb.DateTimeProperty(auto_now_add=True)
 
 class MainPageHandler(webapp2.RequestHandler):
     def get(self):
         template = JINJA_ENVIRONMENT.get_template('main.html')
         self.response.write(template.render())
 
-# class ThesisCpE(webapp2.RequestHandler):
-# 	def get(self):
-# 		template = JINJA_ENVIRONMENT.get_template('main.html')
-#         self.response.write(template.render())
+    def post(self):
+		thesis = Thesis()
+		thesis.year = self.request.get('year')
+		thesis.thesis_title = self.request.get('thesis_title')
+		thesis.abstract = self.request.get('abstract')
+		thesis.adviser = self.request.get('adviser')
+		thesis.section = self.request.get('section')
+		thesis.put()
 
-# 	def post(self):
-# 		thesis = Thesis()
-# 		thesis.year = self.request.get('year')
-# 		thesis.thesis_title = self.request.get('thesis_title')
-# 		thesis.abstract = self.request.get('abstract')
-# 		thesis.adviser = self.request.get('adviser')
-# 		thesis.section = self.request.get('section')
-# 		thesis.put()
+class ThesisCpE(webapp2.RequestHandler):
+	 
+	def get(self):
+        #get all student
+		thesis = Thesis.query().order(-Thesis.date).fetch()
+		thesis_list = []
 
-# 		self.response.headers['Content-Type'] = 'application/json'
-# 		response = {
-# 			'result': 'OK',
-# 			'data': {
-# 			'id': student.key.urlsafe(),
-# 			'year': student.year,
-# 			'thesis_title': student.thesis_title,
-# 			'abstract': student.abstract,
-# 			'adviser': student.adviser,
-# 			'section': student.section,
-# 			}
-# 		}
-# 		self.response.out.write(json.dumps(response))
+		for thesis in thesis:
+			thesis_list.append({
+                    'id' : thesis.key.id(),
+                    'Year' : thesis.Year,
+                    'Title' : thesis.Title,
+                    'Abstract' : thesis.Abstract,
+                    'Adviser' : thesis.Adviser,
+                    'Section' : thesis.Section
+                })
+        #return list to client
+		response = {
+		'result' : 'OK',
+		'data' : thesis_list
+        }
+		self.response.headers['Content-Type'] = 'application/json'
+		self.response.out.write(json.dumps(response))
+
+	def post(self):
+
+		thesis = Thesis()
+		thesis.year = self.request.get('year')
+		thesis.thesis_title = self.request.get('thesis_title')
+		thesis.abstract = self.request.get('abstract')
+		thesis.adviser = self.request.get('adviser')
+		thesis.section = self.request.get('section')
+		thesis.put()
+
+		self.response.headers['Content-Type'] = 'application/json'
+		response = {
+		    'result' : 'OK',
+		    'data': {
+		        'id' : thesis.key.id(),
+		            'year' : thesis.year,
+		            'thesis_title' : thesis.thesis_title,
+		            'abstract' : thesis.abstract,
+		            'adviser' : thesis.adviser,
+		            'section' : thesis.section
+		    }
+		}
+		self.response.out.write(json.dumps(response))
+
+class DeleteInfo(webapp2.RequestHandler):
+    def get(self, studentId):
+        d = Thesis.get_by_id(int(studentId))
+        d.key.delete()
+        self.redirect('/')
 
 app = webapp2.WSGIApplication([
-    ('/api/thesis', ThesisCpE),
-    ('/home', MainPageHandler),
-    ('/', MainPageHandler)
+('/api/thesis', ThesisCpE),
+('/thesis/delete/(.*)', DeleteInfo),
+('/home', MainPageHandler),
+('/', MainPageHandler)
 ], debug=True)
--- static/script.js
+++ static/script.js
@@ -1,5 +1,52 @@
 (function($) {
 
     // your code here
+	function onFormSubmit(event){
+
+		var data = $(event.target).serializeArray();
+
+		var thesis = {};
+		for(var i = 0; i<data.length ; i++){
+			thesis[data[i].name] = data[i].value;
+		}
+
+		// send data to server
+			var thesis_create_api = '/api/thesis';
+			$.post(thesis_create_api, thesis, function(response){
+
+			// read response from server
+			if (response.status = 'OK') {
+				var thesis_info = response.data.year + ' - ' + response.data.thesis_title + ' <a href=\"/thesis/delete/'+response.data.id+'\"><button id=\"delete\" type=\"submit\">DELETE</button></a><hr> ';
+				$('.thesis-list').prepend('<li>' + thesis_info + '</li>')
+				$('input[type=text], [type=number]').val('');
+				$('select[name=year]').val('year');
+				$('select[name=section]').val('section');
+			} else {
+				// add something 
+			}
+
+			});
+
+		return false;
+	}
+
+	function loadThesis(){
+		var thesis_list_api = '/api/thesis';
+		$.get(thesis_list_api, {} , function(response) {
+			console.log('.thesis-list', response)
+			response.data.forEach(function(thesis){
+				var thesis_info = thesis.year + ' - '  + thesis.thesis_title + ' <a href=\"/thesis/delete/'+thesis.id+'\"><button id=\"delete\" type=\"submit\">DELETE</button></a><hr> ';
+				$('.thesis-list').append('<li>' + thesis_info + '</li>')
+			});
+		});
+	}
+
+	loadThesis();
+	$('form#form1').submit(onFormSubmit);
+
+	$(document).on('click', 'button#delete', function(){
+		$(this).closest('li').remove();
+	});
+
 
 })(jQuery)
